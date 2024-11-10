587bac2bbb1406b2ce56143f251ffd456d8d64ef
kate <kate@dariox.club>
2024-11-10 15:38:28 +0800

    Created ClientApiKeyModel.cs

---
+++ Adastral.Cockatoo.DataAccess/Models/ClientApiKeyModel.cs
@@ -0,0 +1,117 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adastral.Cockatoo.DataAccess.Models
+{
+    public class ClientApiKeyModel : BaseGuidModel
+    {
+        public const string CollectionName = "clientApiKey";
+
+        public ClientApiKeyModel()
+            : base()
+        {
+            UserId = "00000000-0000-0000-0000-000000000000";
+            ClientKeyProivderId = "00000000-0000-0000-0000-000000000000";
+            Enabled = true;
+            CreatedAt = new BsonTimestamp(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+            Token = GenerateToken();
+        }
+
+        private static string GenerateToken()
+        {
+            var rand = new Random();
+            var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.=";
+            return new string(Enumerable
+                .Range(0, 32)
+                .Select(num => characters[rand.Next() % characters.Length])
+                .ToArray());
+        }
+
+        /// <summary>
+        /// Foreign Key to <see cref="UserModel"/>
+        /// </summary>
+        [DefaultValue("00000000-0000-0000-0000-000000000000")]
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Foreign Key to <see cref="ClientApiKeyProviderModel"/>
+        /// </summary>
+        [DefaultValue("00000000-0000-0000-0000-000000000000")]
+        public string ClientKeyProivderId { get; set; }
+
+        /// <summary>
+        /// When <see langword="false"/>, <see cref="DisableReason"/> must be set.
+        /// </summary>
+        [DefaultValue(true)]
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// Token that was generated.
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// When this document was created. Unix Timestamp (Seconds, UTC)
+        /// </summary>
+        public BsonTimestamp CreatedAt { get; set; }
+
+        /// <summary>
+        /// Timestamp when this document was disabled. Unix Timestamp (Seconds, UTC)
+        /// </summary>
+        [BsonIgnoreIfNull]
+        public BsonTimestamp? DisabledAt { get; set; }
+
+        /// <summary>
+        /// Why this Token was disabled. 
+        /// </summary>
+        [BsonIgnoreIfNull]
+        [BsonRepresentation(BsonType.String)]
+        public ClientAuthorizationTokenDisableReason? DisableReason { get; set; }
+
+        /// <summary>
+        /// Will be populated when <see cref="DisableReason"/> is set to <see cref="ClientAuthorizationTokenDisableReason.Other"/>
+        /// </summary>
+        [BsonIgnoreIfNull]
+        [BsonIgnoreIfDefault]
+        public string? DisableReasonOther { get; set; }
+
+        /// <summary>
+        /// User that disabled this token. When <see langword="null"/>, then assume that it is the user that this 
+        /// was created for (<see cref="UserId"/>)
+        /// </summary>
+        /// <remarks>
+        /// Foreign Key to <see cref="UserModel"/>
+        /// </remarks>
+        [BsonIgnoreIfNull]
+        [BsonIgnoreIfDefault]
+        public string? DisabledByUserId { get; set; }
+    }
+    public enum ClientAuthorizationTokenDisableReason
+    {
+        /// <summary>
+        /// User requested to be logged out, so the token was destroyed.
+        /// </summary>
+        Logout,
+        
+        /// <summary>
+        /// User did not authenticate with this token within the last month.
+        /// </summary>
+        Inactive,
+        
+        /// <summary>
+        /// Account was manually disabled by a system administrator.
+        /// </summary>
+        AccountDisabled,
+
+        /// <summary>
+        /// Other reason, reason should be specified in <see cref="ClientApiKeyModel.DisableReasonOther"/>
+        /// </summary>
+        Other
+    }
+}
---
+++ Adastral.Cockatoo.DataAccess/Repositories/ClientApiKeyRepository.cs
@@ -0,0 +1,78 @@
+﻿using Adastral.Cockatoo.Common;
+using Adastral.Cockatoo.DataAccess.Models;
+using MongoDB.Driver;
+using NLog;
+
+namespace Adastral.Cockatoo.DataAccess.Repositories
+{
+    [CockatooDependency]
+    public class ClientApiKeyRepository : BaseRepository<ClientApiKeyModel>
+    {
+        private readonly Logger _log = LogManager.GetCurrentClassLogger();
+        public ClientApiKeyRepository(IServiceProvider services)
+            : base(ClientApiKeyModel.CollectionName, services)
+        {
+            var collection = GetCollection();
+            if (collection != null)
+            {
+                if (!collection.Indexes.List().Any())
+                {
+                    var dataKeys = Builders<ClientApiKeyModel>
+                        .IndexKeys
+                        .Ascending(v => v.Id)
+                        .Ascending(v => v.Token)
+                        .Ascending(v => v.UserId);
+                    var adminKeys = Builders<ClientApiKeyModel>
+                        .IndexKeys
+                        .Ascending(v => v.DisabledByUserId);
+                    collection.Indexes.CreateOne(new CreateIndexModel<ClientApiKeyModel>(dataKeys));
+                    collection.Indexes.CreateOne(new CreateIndexModel<ClientApiKeyModel>(adminKeys));
+                    _log.Info($"Added indexes");
+                }
+            }
+        }
+
+        public async Task<ClientApiKeyModel?> GetById(string id)
+        {
+            var filter = Builders<ClientApiKeyModel>
+                .Filter
+                .Where(v => v.Id == id);
+            var result = await BaseFind(filter);
+            return result?.FirstOrDefault();
+        }
+
+        public async Task<List<ClientApiKeyModel>?> GetManyByUser(string userId, bool includeDisabled = false)
+        {
+            if (string.IsNullOrEmpty(userId))
+                return [];
+            var filter = Builders<ClientApiKeyModel>
+                .Filter
+                .Where(v => v.UserId == userId);
+            if (!includeDisabled)
+            {
+                filter &= Builders<ClientApiKeyModel>
+                    .Filter
+                    .Where(v => v.Enabled);
+            }
+            var result = await BaseFind(filter);
+            return result?.ToList() ?? [];
+        }
+
+        public async Task<ClientApiKeyModel?> GetByToken(string token, bool includeDisabled = false)
+        {
+            if (string.IsNullOrEmpty(token))
+                return null;
+            var filter = Builders<ClientApiKeyModel>
+                .Filter
+                .Where(v => v.Token == token);
+            if (!includeDisabled)
+            {
+                filter &= Builders<ClientApiKeyModel>
+                    .Filter
+                    .Where(v => v.Enabled);
+            }
+            var result = await BaseFind(filter);
+            return result?.FirstOrDefault();
+        }
+    }
+}
