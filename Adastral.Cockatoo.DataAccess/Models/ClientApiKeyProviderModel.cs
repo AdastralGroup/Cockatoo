b14b190cf77bb7437b3f6ae5a26b465d07290f29
kate <kate@dariox.club>
2024-11-10 15:38:16 +0800

    Created ClientApiKeyProviderModel

---
+++ Adastral.Cockatoo.DataAccess/Models/ClientApiKeyProviderModel.cs
@@ -0,0 +1,91 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adastral.Cockatoo.DataAccess.Models
+{
+    public class ClientApiKeyProviderModel : BaseGuidModel
+    {
+        public const string CollectionName = "clientApiKeyProvider";
+
+        public ClientApiKeyProviderModel()
+            : base()
+        {
+            DisplayName = Id;
+            CreatedAt = new BsonTimestamp(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+            IsActive = false;
+            PublicKeyXml = "";
+            PrivateKeyXml = "";
+
+            if (string.IsNullOrEmpty(PublicKeyXml) || string.IsNullOrEmpty(PrivateKeyXml))
+            {
+                GenerateKeyPair();
+            }
+        }
+
+        private void GenerateKeyPair()
+        {
+            var data = RSA.Create(2048);
+            var privateKey = data.ToXmlString(true);
+            var publicKey = data.ToXmlString(false);
+            if (string.IsNullOrEmpty(privateKey) || string.IsNullOrEmpty(publicKey))
+            {
+                if (string.IsNullOrEmpty(privateKey) && !string.IsNullOrEmpty(publicKey))
+                {
+                    throw new InvalidOperationException($"Unable to {nameof(GenerateKeyPair)} since the result of {typeof(RSA)}.{nameof(data.ToXmlString)}(true) returned a string that's null or empty");
+                }
+                else if (!string.IsNullOrEmpty(privateKey) && string.IsNullOrEmpty(publicKey))
+                {
+                    throw new InvalidOperationException($"Unable to {nameof(GenerateKeyPair)} since the result of {typeof(RSA)}.{nameof(data.ToXmlString)}(false) returned a string that's null or empty");
+
+                }
+                else if (string.IsNullOrEmpty(privateKey) && string.IsNullOrEmpty(publicKey))
+                {
+                    throw new InvalidOperationException($"Unable to {nameof(GenerateKeyPair)} since the result of {typeof(RSA)}.{nameof(data.ToXmlString)}(false) and {typeof(RSA)}.{nameof(data.ToXmlString)}(true) returned a string that's null or empty");
+                }
+            }
+            PublicKeyXml = publicKey;
+            PrivateKeyXml = privateKey;
+        }
+
+        /// <summary>
+        /// Display Name for the Client Key Provider
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Timestamp when this document was created. Unix Timestamp (UTC, Seconds)
+        /// </summary>
+        public BsonTimestamp CreatedAt { get; set; }
+
+        /// <summary>
+        /// Foreign Key to <see cref="StorageFileModel"/>
+        /// </summary>
+        public string? BrandIconFileId { get; set; }
+
+        /// <summary>
+        /// Should any requests that were generated with this Client Key Provider be respected?
+        /// Defaults to false, which means the auth key provided should be ignored.
+        /// </summary>
+        [DefaultValue(false)]
+        public bool IsActive { get; set; } = false;
+
+        /// <summary>
+        /// Result of <see cref="RSA.ToXmlString(bool)"/> where the bool parameter is false.
+        /// </summary>
+        /// <remarks>
+        /// Used for encrypting user credentials when being passed through to the REST API, which will then generate a client authorization token.
+        /// </remarks>
+        public string PublicKeyXml { get; set; }
+
+        /// <summary>
+        /// Result of <see cref="RSA.ToXmlString(bool)"/> where the bool parameter is true.
+        /// </summary>
+        public string PrivateKeyXml { get; set; }
+    }
+}
---
+++ Adastral.Cockatoo.DataAccess/Repositories/ClientApiKeyProviderRepository.cs
@@ -0,0 +1,115 @@
+﻿using Adastral.Cockatoo.Common;
+using Adastral.Cockatoo.DataAccess.Models;
+using MongoDB.Driver;
+using NLog;
+using System.Data;
+
+namespace Adastral.Cockatoo.DataAccess.Repositories
+{
+    [CockatooDependency]
+    public class ClientApiKeyProviderRepository : BaseRepository<ClientApiKeyProviderModel>
+    {
+        private readonly Logger _log = LogManager.GetCurrentClassLogger();
+        public ClientApiKeyProviderRepository(IServiceProvider services)
+            : base(ClientApiKeyProviderModel.CollectionName, services)
+        {
+            var collection = GetCollection();
+            if (collection != null)
+            {
+                if (!collection.Indexes.List().Any())
+                {
+                    var dataKeys = Builders<ClientApiKeyProviderModel>
+                        .IndexKeys
+                        .Ascending(v => v.Id)
+                        .Ascending(v => v.IsActive);
+                    collection.Indexes.CreateOne(new CreateIndexModel<ClientApiKeyProviderModel>(dataKeys));
+                    _log.Info($"Added indexes");
+                }
+            }
+        }
+
+        public async Task<ClientApiKeyProviderModel?> GetById(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return null;
+            var filter = Builders<ClientApiKeyProviderModel>
+                .Filter
+                .Where(v => v.Id == id);
+            var result = await BaseFind(filter);
+            return result?.FirstOrDefault();
+        }
+        
+        public async Task<List<ClientApiKeyProviderModel>> GetAll()
+        {
+            var filter = Builders<ClientApiKeyProviderModel>
+                .Filter
+                .Empty;
+            var result = await BaseFind(filter);
+            return result?.ToList() ?? [];
+        }
+
+        public async Task<long> Delete(params string[] ids)
+        {
+            var collection = GetCollection();
+            if (collection == null)
+                throw new NoNullAllowedException($"{nameof(GetCollection)} returned null");
+            var filter = Builders<ClientApiKeyProviderModel>
+                .Filter
+                .In(v => v.Id, ids);
+            var result = await collection.DeleteManyAsync(filter);
+            return result.DeletedCount;
+        }
+        public async Task<long> Count()
+        {
+            var collection = GetCollection();
+            if (collection == null)
+                throw new NoNullAllowedException($"{nameof(GetCollection)} returned null");
+            var filter = Builders<ClientApiKeyProviderModel>
+                .Filter.Empty;
+            var result = await collection.CountDocumentsAsync(filter);
+            return result;
+        }
+        public async Task<long> CountWhereId(string id)
+        {
+            var collection = GetCollection();
+            if (collection == null)
+                throw new NoNullAllowedException($"{nameof(GetCollection)} returned null");
+            var filter = Builders<ClientApiKeyProviderModel>
+                .Filter
+                .Where(v => v.Id == id);
+            var result = await collection.CountDocumentsAsync(filter);
+            return result;
+        }
+
+        public async Task<ClientApiKeyProviderModel> InsertOrUpdate(ClientApiKeyProviderModel model)
+        {
+            var collection = GetCollection();
+            if (collection == null)
+                throw new NoNullAllowedException($"{nameof(GetCollection)} returned null");
+
+            var existingIdCount = await CountWhereId(model.Id);
+            if (existingIdCount < 1)
+            {
+                _log.Info($"Inserted document with Id {model.Id}");
+                await collection.InsertOneAsync(model);
+                return model;
+            }
+            else
+            {
+                var updateDefinition = Builders<ClientApiKeyProviderModel>
+                    .Update
+                    .Set(v => v.DisplayName, model.DisplayName)
+                    .Set(v => v.BrandIconFileId, model.BrandIconFileId)
+                    .Set(v => v.IsActive, model.IsActive)
+                    .Set(v => v.PublicKeyXml, model.PublicKeyXml)
+                    .Set(v => v.PrivateKeyXml, model.PrivateKeyXml);
+                var updateFilter = Builders<ClientApiKeyProviderModel>
+                    .Filter
+                    .Where(v => v.Id == model.Id);
+                var result = await collection.UpdateManyAsync(updateFilter, updateDefinition);
+                _log.Info($"Updated {result.ModifiedCount} records with Id {model.Id}");
+                return model;
+            }
+        }
+    }
+}
